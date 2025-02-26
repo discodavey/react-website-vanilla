3eb4c308f068549f5e36d370ea8117ad5f7c7c84
Dave <discodavey@gmail.com>
2025-02-26 10:28:47 +0000

    Add home content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <h1>This is a test</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
+      <h1>This is a burger</h1>
 
     </div>
   );
