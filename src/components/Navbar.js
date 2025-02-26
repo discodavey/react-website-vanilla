75662f9a9d682b6e044c9c5f2f4c4d1a4800bbd5
Dave <discodavey@gmail.com>
2025-02-26 08:49:01 +0000

    Add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import logo from '../assets/images/impulse-logo-1.svg';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div className="navigation">
-      <div className="left-column"></div>
-      <div className="right-column">
+      <div className="left-column">
         <img src={logo} alt="Impulse Radio" />
+      </div>
+      <div className="right-column">
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
       </div>
     </div>
   )
